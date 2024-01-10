3dc26c7cbf42baa3a6a7d6ae83bb137059242d83
aswinraja02 <aswin.raja@testhouse.net>
2024-01-10 20:41:33 +0530

    data changed

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -153,9 +153,9 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
+            &copy; 2023 Tech Tac Solutions. Design by{" "}
+            <a href="https://aswinraja.netlify.app" rel="nofollow" target="blank">
+              ASWIN RAJA A
             </a>
           </p>
         </div>
